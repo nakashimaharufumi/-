bea5fc7ab5b0bc8f9b0473ae691b0c5cad9425ba
Nakashima Harufumi <nakashimaharufumi@gmail.com>
2022-06-14 23:09:48 +0900

    種類違い実装

--- tetris.js
+++ tetris.js
@@ -28,6 +28,7 @@ function writeField() {
     line(440, 40, 440, 640);
     for (let i=1; i<10; i++) line((i+1)*40, 40, (i+1)*40, 640);
     for (let i=1; i<15; i++) line(40, (i+1)*40, 440, (i+1)*40);
+    fill(255);
     textSize(30);
     text("count: "+parseInt(count/60), 50, 30);
     text("score: "+score, 200, 30)
@@ -36,6 +37,7 @@ function writeField() {
 let cs = 0;
 let score = 0;
 let count = 0;
+let bl;
 let masu = [];
 let kesu = [];
 
@@ -48,9 +50,35 @@ function draw() {
 
 //パズル描写
 function drawPzl() {
+    let col;
+
     for (let y=1; y<16; y++) {
         for (let x=1; x<11; x++) {
-            if (masu[y][x]==1) square(40*x, 40*y, 40);
+            if (masu[y][x]==1) {
+                col = color(255, 0, 0);
+                fill(col);
+                square(40*x, 40*y, 40);
+            } else if (masu[y][x]==2) {
+                col = color(0, 255, 0);
+                fill(col);
+                square(40*x, 40*y, 40);
+            } else if (masu[y][x]==3) {
+                col = color(0, 0, 255);
+                fill(col);
+                square(40*x, 40*y, 40);
+            } else if (masu[y][x]==4) {
+                col = color(255, 0, 255);
+                fill(col);
+                square(40*x, 40*y, 40);
+            } else if (masu[y][x]==5) {
+                col = color(255, 255, 0);
+                fill(col);
+                square(40*x, 40*y, 40);
+            } else if (masu[y][x]==6) {
+                col = color(0, 255, 255);
+                fill(col);
+                square(40*x, 40*y, 40);
+            }
         }
     }
 }
@@ -60,24 +88,28 @@ let move = 0; //ブロック移動の場合分け用変数
 //ブロック移動
 function moveBlock() {
     switch (move) {
-        case 0: //ブロック操作
-            masu[myBlockY][myBlockX] = 1;
-            
+        case 0: //ブロック決定
+            bl = parseInt(random(1, 7));
+            move = 1;
+        break;
+        case 1: //ブロック操作
+            masu[myBlockY][myBlockX] = bl;
+
             if ((keyIsPressed && keyCode==DOWN_ARROW) || count%(60*5)==0) { //下移動
                 keyIsPressed = false;
                 if (masu[myBlockY+1][myBlockX]==0) {
                     myBlockY++;
-                    masu[myBlockY][myBlockX] = 1;
+                    masu[myBlockY][myBlockX] = bl;
                     masu[myBlockY-1][myBlockX] = 0;
                 } else {
-                    move = 1;
+                    move = 2;
                 }
             }
             if (keyIsPressed && keyCode==RIGHT_ARROW) { //右移動
                 keyIsPressed = false;
                 if (masu[myBlockY][myBlockX+1]==0) {
                     myBlockX++;
-                    masu[myBlockY][myBlockX] = 1;
+                    masu[myBlockY][myBlockX] = bl;
                     masu[myBlockY][myBlockX-1] = 0;
                 }
             }
@@ -85,27 +117,27 @@ function moveBlock() {
                 keyIsPressed = false;
                 if (masu[myBlockY][myBlockX-1]==0) {
                     myBlockX--;
-                    masu[myBlockY][myBlockX] = 1;
+                    masu[myBlockY][myBlockX] = bl;
                     masu[myBlockY][myBlockX+1] = 0;
                 }
             }
         break;
-        case 1: //列が揃っているか判定
+        case 2: //列が揃っているか判定
             for (let y=16; y>0; y--) {
-                if (masu[y][1]+masu[y][2]+masu[y][3]+masu[y][4]+masu[y][5]+masu[y][6]+masu[y][7]+masu[y][8]+masu[y][9]+masu[y][10]==10) {
+                if (masu[y][1]>0 && masu[y][2]>0 && masu[y][3]>0 && masu[y][4]>0 && masu[y][5]>0 && masu[y][6]>0 && masu[y][7]>0 && masu[y][8]>0 && masu[y][9]>0 && masu[y][10]>0) {
                     kesu[y] = 1;
                 } else kesu[y] = 0;
             }
             let n = 0;
             for (let y=16; y>0; y--) if (kesu[y]>0) n++
-            if (n>0) move = 2;
+            if (n>0) move = 3;
             else {
                 myBlockX = 5;
                 myBlockY = 1;
                 move = 0;
             }
         break;
-        case 2: //ブロック削除
+        case 3: //ブロック削除
             for (y=16; y>0; y--) {
                 if (kesu[y]>0) {
                     kesu[y] = 0;
@@ -113,9 +145,9 @@ function moveBlock() {
                     for (x=11;x>0; x--) masu[y][x] = 0;
                 }
             }
-            move = 3;
+            move = 4;
         break;
-        case 3: //ブロック全体を落とす
+        case 4: //ブロック全体を落とす
             let c = 0;
             for (let y=16; y>0; y--) {
                 for (let x=10; x>0; x--) {
@@ -126,7 +158,20 @@ function moveBlock() {
                     }
                 }
             }
-            if (c==0) move = 1;
+            if (c==0) move = 2;
         break;
     }
 }
+
+/*
+class Block {
+    constructor() {
+        this.x = myBlockX;
+        this.y = myBlockY;
+    }
+
+    make() {
+        masu[y][x] = 1;
+    }
+}
+*/
